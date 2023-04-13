4026d734c58786610e4df48630c4a1551036c42f
Josh Soref <2119212+jsoref@users.noreply.github.com>
2023-04-13 18:34:57 -0400

    spelling: indentation
    Signed-off-by: Josh Soref <2119212+jsoref@users.noreply.github.com>

--- tests/source/issue-5042/multi-line_comment_without_trailing_comma.rs
+++ tests/source/issue-5042/multi-line_comment_without_trailing_comma.rs
@@ -1,6 +1,6 @@
 fn main() {
     // 5042 deals with trailing commas, not the indentation issue of these comments
-    // When a future PR fixes the inentation issues these test can be updated
+    // When a future PR fixes the indentation issues these test can be updated
     let _ = std::ops::Add::add(10, 20
         // ...
         // ...
