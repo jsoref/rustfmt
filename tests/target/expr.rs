37cb8d0b8fb1a8cffd07ad4e357293583cc7e1da
Josh Soref <2119212+jsoref@users.noreply.github.com>
2023-04-13 18:34:58 -0400

    spelling: unexpectedly
    Signed-off-by: Josh Soref <2119212+jsoref@users.noreply.github.com>

--- tests/target/expr.rs
+++ tests/target/expr.rs
@@ -550,18 +550,22 @@ fn issue3226() {
     {
         {
             {
-                return Err(
-                    ErrorKind::ManagementInterfaceError("Server exited unexpectedly").into(),
-                );
+                {
+                    return Err(
+                        ErrorKind::ManagementInterfaceError("Server exited unexpectedly").into(),
+                    );
+                }
             }
         }
     }
     {
         {
             {
-                break Err(
-                    ErrorKind::ManagementInterfaceError("Server exited unexpectedlyy").into(),
-                );
+                {
+                    break Err(
+                        ErrorKind::ManagementInterfaceError("Server exited unexpectedly").into(),
+                    );
+                }
             }
         }
     }
