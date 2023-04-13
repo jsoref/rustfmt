53b8819497cad3e8b2edd66eaa51b7c208609558
Josh Soref <2119212+jsoref@users.noreply.github.com>
2023-04-13 18:34:57 -0400

    spelling: fifteen
    Signed-off-by: Josh Soref <2119212+jsoref@users.noreply.github.com>

--- tests/target/issue-3304.rs
+++ tests/target/issue-3304.rs
@@ -1,9 +1,9 @@
 // rustfmt-error_on_line_overflow: true
 
-#[rustfmt::skip] use one::two::three::four::five::six::seven::eight::night::ten::eleven::twelve::thirteen::fourteen::fiveteen;
+#[rustfmt::skip] use one::two::three::four::five::six::seven::eight::night::ten::eleven::twelve::thirteen::fourteen::fifteen;
 #[rustfmt::skip]
 
-use one::two::three::four::five::six::seven::eight::night::ten::eleven::twelve::thirteen::fourteen::fiveteen;
+use one::two::three::four::five::six::seven::eight::night::ten::eleven::twelve::thirteen::fourteen::fifteen;
 
 macro_rules! test_macro {
     ($($id:ident),*) => {};
@@ -16,17 +16,17 @@ macro_rules! test_macro2 {
 }
 
 fn main() {
-    #[rustfmt::skip] test_macro! { one, two, three, four, five, six, seven, eight, night, ten, eleven, twelve, thirteen, fourteen, fiveteen };
+    #[rustfmt::skip] test_macro! { one, two, three, four, five, six, seven, eight, night, ten, eleven, twelve, thirteen, fourteen, fifteen };
     #[rustfmt::skip]
     
-    test_macro! { one, two, three, four, five, six, seven, eight, night, ten, eleven, twelve, thirteen, fourteen, fiveteen };
+    test_macro! { one, two, three, four, five, six, seven, eight, night, ten, eleven, twelve, thirteen, fourteen, fifteen };
 }
 
 fn test_local() {
-    #[rustfmt::skip] let x = test_macro! { one, two, three, four, five, six, seven, eight, night, ten, eleven, twelve, thirteen, fourteen, fiveteen };
+    #[rustfmt::skip] let x = test_macro! { one, two, three, four, five, six, seven, eight, night, ten, eleven, twelve, thirteen, fourteen, fifteen };
     #[rustfmt::skip]
     
-    let x = test_macro! { one, two, three, four, five, six, seven, eight, night, ten, eleven, twelve, thirteen, fourteen, fiveteen };
+    let x = test_macro! { one, two, three, four, five, six, seven, eight, night, ten, eleven, twelve, thirteen, fourteen, fifteen };
 }
 
 fn test_expr(_: [u32]) -> u32 {
@@ -36,7 +36,7 @@ fn test_expr(_: [u32]) -> u32 {
     test_expr([9999999999999, 9999999999999, 9999999999999, 9999999999999, 9999999999999, 9999999999999, 9999999999999, 9999999999999])
 }
 
-#[rustfmt::skip] mod test { use one::two::three::four::five::six::seven::eight::night::ten::eleven::twelve::thirteen::fourteen::fiveteen; }
+#[rustfmt::skip] mod test { use one::two::three::four::five::six::seven::eight::night::ten::eleven::twelve::thirteen::fourteen::fifteen; }
 #[rustfmt::skip]
 
-mod test { use one::two::three::four::five::six::seven::eight::night::ten::eleven::twelve::thirteen::fourteen::fiveteen; }
+mod test { use one::two::three::four::five::six::seven::eight::night::ten::eleven::twelve::thirteen::fourteen::fifteen; }
